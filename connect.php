6d812c99f2ea3b17aa1b340b1dff4cf281f4fdbc
Albert Kulas <157285246+Quebotek@users.noreply.github.com>
2025-03-29 17:08:59 +0100

    Ajout env

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
 	    define ('SERVEUR_BD','c_mariadb:3306');
-	    define ('LOGIN_BD','userbd');
-	    define ('PASS_BD','password');
-	    define ('NOM_BD','Pokemon'); 
+	    define ('LOGIN_BD',getenv('USER_BD'));
+	    define ('PASS_BD',getenv('USER_PWD'));
+	    define ('NOM_BD',getenv('NOM_BD')); 
